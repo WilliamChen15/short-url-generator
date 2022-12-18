5714f602c6711ece3d795d1da9604419c8f77b12
WilliamChen15 <turelight13@gmail.com>
2022-12-18 17:04:17 +0800

    feat: home page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,5 +3,8 @@ const router = express.Router()
 
 const URL = require('../../models/URL')
 
+router.get('/', (req, res) => {
+  res.render('index')
+})
 
 module.exports = router
--- routes/modules/show.js
+++ routes/modules/show.js
@@ -3,5 +3,12 @@ const router = express.Router()
 
 const URL = require('../../models/URL')
 
+// router.get('/', (req, res) => {
+//   const originalURL = req.body.originalURL
+//   URL.find()
+//     .lean()
+//     .then(URLinDB => )
+//     .catch(error => console.error(error))
+// })
 
 module.exports = router
