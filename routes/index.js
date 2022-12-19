d8efa9041737d0d39458e3a264073dcf623070d0
WilliamChen15 <turelight13@gmail.com>
2022-12-19 12:58:01 +0800

    feat: show page

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const home = require('./modules/home')
-const show = require('./modules/show')
 
-router.use('/show', show)
 router.use('/', home)
 
 module.exports = router
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -7,4 +7,18 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
+router.post('/show', (req, res) => {
+  const originalURL = req.body.originalURL
+  res.redirect('/show')
+})
+
+router.get('/show', (req, res) => {
+
+  URL.find()
+    .lean()
+    // .then(URLinDB => )
+    .then(res.render('show'))
+    .catch(error => console.error(error))
+})
+
 module.exports = router
